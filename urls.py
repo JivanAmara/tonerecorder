b2c67d95a403f0a947f6630b1344684ae6a4b14e
Jivan Amara <Development@JivanAmara.net>
2016-03-13 11:12:28 -0700

    Fixed template url tag missing quotes around view name. Updated urls.py to conform to expected format for Django 1.10.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url
+
 from tonerecorder.views import hello_world, whatd_you_say
 from tonerecorder.views import handle_upload
 from tonerecorder.views import makefile
@@ -7,17 +8,17 @@ import os
 CODEBASE_DIR = os.path.dirname(__file__) \
     + '/listenup_20100910/listenup/codebase/'
 
-urlpatterns = patterns('',
-    (r'makefile/(.*?)$', makefile),
-    (r'play', hello_world),
-    (r'record', whatd_you_say),
-    (r'codebase/(.*)', 'django.views.static.serve'
-        , {'document_root': CODEBASE_DIR, 'show_indexes': True} ),
-#    (r'^audio/(.*)', 'django.views.static.serve'
+urlpatterns = (
+    url(r'makefile/(.*?)$', makefile),
+    url(r'play', hello_world),
+    url(r'record', whatd_you_say),
+    url(r'codebase/(.*)', 'django.views.static.serve'
+        , {'document_root': CODEBASE_DIR, 'show_indexes': True}),
+#    url(r'^audio/(.*)', 'django.views.static.serve'
 #        , {'document_root': AUDIOFILE_DIR, 'show_indexes': True} ),
-    (r'handle_upload', handle_upload)
+    url(r'handle_upload', handle_upload)
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
