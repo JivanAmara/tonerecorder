c7bf66490f449c50afbac3f52590d8c484dba837
Jivan Amara <Development@JivanAmara.net>
2016-03-13 21:08:46 -0700

    Audio file recording & upload working for mobiles using html5 (8kHz).

--- urls.py
+++ urls.py
@@ -1,27 +1,7 @@
 from django.conf.urls import url
-
-from tonerecorder.views import hello_world, whatd_you_say
-from tonerecorder.views import handle_upload
-from tonerecorder.views import makefile
-import os
-
-CODEBASE_DIR = os.path.dirname(__file__) \
-    + '/listenup_20100910/listenup/codebase/'
+from tonerecorder.views import MobileRecordView, AudioUploadView
 
 urlpatterns = (
-    url(r'makefile/(.*?)$', makefile),
-    url(r'play', hello_world),
-    url(r'record', whatd_you_say),
-    url(r'codebase/(.*)', 'django.views.static.serve'
-        , {'document_root': CODEBASE_DIR, 'show_indexes': True}),
-#    url(r'^audio/(.*)', 'django.views.static.serve'
-#        , {'document_root': AUDIOFILE_DIR, 'show_indexes': True} ),
-    url(r'handle_upload', handle_upload)
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'record-mobile', MobileRecordView.as_view(), name='tonerecorder_record-mobile'),
+    url(r'upload-wav', AudioUploadView.as_view(), name='tonerecorder_upload-wav'),
 )
--- views.py
+++ views.py
@@ -1,102 +1,41 @@
 # coding=utf-8
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.core.urlresolvers import reverse
+from django.shortcuts import render
+
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
 
 import sys
 import random
 import os
+from django.views.generic import View
+from django.http.response import HttpResponseRedirect
 
 AUDIOFILE_DIR = os.path.join(os.path.dirname(__file__), 'audio_files')
 
-def hello_world(request):
-    resp = HttpResponse(
-               '<head></head>'
-               '<body><h1>Hello World!!!</h1>'
-                   '<applet '
-                       'code="com.softsynth.javasonics.recplay.PlayerApplet"'
-                       'codebase="/tonerecorder/codebase/"'
-                       'archive="JavaSonicsListenUp.jar"'
-                       'name="ListenUpPlayer"'
-                       'width="400"'
-                       'height="120">'
-                       #<!-- Play immediately without waiting for button press. -->
-                       '<param name="autoPlay" value="yes">'
-                       #<!-- Play the file at this URL. -->
-                       '<param name="sampleURL" value="/audio/welcome.wav">'
-                   '</applet>'
-               '</body>'
-           )
+class MobileRecordView(View):
+    def get(self, request):
+        context = RequestContext(request)
+        resp = render(request, 'record-html5-mobile.html')
+        return resp
 
-    return resp
+class AudioUploadView(View):
+    def post(self, request):
+        if len(request.FILES.items()) != 1:
+            resp = HttpResponseRedirect(request.META['HTTP_REFERER'])
+        else:
+            filename, file = request.FILES.items()[0]
+            rint = random.randint(1, sys.maxint)
+            new_filename = 'uploaded_file_' + str(rint) + '.wav'
+            new_fullpath = os.path.join(
+                               AUDIOFILE_DIR, new_filename
+                           )
+            destination = open(new_fullpath, 'wb+')
+            for chunk in file.chunks():
+                destination.write(chunk)
+            destination.close()
+            msg = 'Saved as "{}".  Go back to recording page with new syllable queue.'\
+                      .format(new_fullpath)
+            resp = HttpResponse(msg)
 
-@login_required
-def whatd_you_say(request):
-    rc = RequestContext(request)
-    resp = render_to_response('record.html', rc)
-
-    return resp
-
-@login_required
-@csrf_exempt
-def handle_upload(request):
-    resp = "CALLJS alert('Server says Yo.');\n"
-    resp += "CALLJS say_hi();\n"
-    resp += "SUCCESS"
-    print("Received POST.")
-    print("Files in post: \n")
-    print(request.FILES.keys())
-    filename, file = request.FILES.items()[0]
-    rint = random.randint(1, sys.maxint)
-    
-    new_filename = 'uploaded_file_' + str(rint) + '.wav'
-    new_fullpath = os.path.join(
-                       AUDIOFILE_DIR, new_filename
-                   )
-    destination = open(new_fullpath, 'wb+')
-    for chunk in file.chunks():
-        destination.write(chunk)
-    destination.close()
-
-    return HttpResponse(resp)
-
-def handle_uploaded_file(up_file):
-    print("Got it!")
-    fullpath = os.path.join(AUDIOFILE_DIR, 'upload.wav')
-    ondisk_file = open(fullpath, "wb")
-    ondisk_file.write(up_file.read())
-    ondisk_file.close()
-
-def makefile(request, filename):
-    fullpath = os.path.join(AUDIOFILE_DIR, filename)
-    ondisk_file = open(fullpath, "wb")
-    ondisk_file.write(str(random.randint(0, 254)))
-    ondisk_file.close()
-    return HttpResponse(fullpath)
-
-#from django import forms
-#class UploadFileForm(forms.Form):
-#    title = forms.CharField(max_length=50)
-#    file  = forms.FileField()
-
-"""
-def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        print("Number of files: {0}".format(len(request.FILES)))
-        for filename in request.FILES.keys():
-            print(filename)
-            
-#        if form.is_valid():
-        handle_uploaded_file(request.FILES['userfile'])
-        return HttpResponseRedirect(reverse(upload_file))
-    else:
-        form = UploadFileForm()
-    return render_to_response('upload.html'
-                              , {'form': form}
-                              ,context_instance=RequestContext(request)
-    )
-"""
+        return resp
