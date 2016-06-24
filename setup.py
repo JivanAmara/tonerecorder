d34963e362a88e430e8cc79a606bcd0372efc82c
Jivan Amara <Development@JivanAmara.net>
2016-06-24 15:33:05 -0700

    Bumped version to 0.4.1. Tagged as v0.4.1.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', 'r') as rmf:
 
 setup(
     name="tonerecorder",
-    version="0.4.0",
+    version="0.4.1",
     author="Jivan Amara",
     author_email="Development@JivanAmara.net",
     packages=['tonerecorder', 'tonerecorder.migrations',
